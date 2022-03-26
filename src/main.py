f977e90afb6e384c510b292b0f5a0964ae9951c9
cmmm976 <icdk5b4zf@relay.firefox.com>
2022-03-26 13:49:39 +0100

    add number of players choice

--- src/main.py
+++ src/main.py
@@ -4,10 +4,8 @@ from jeu import Jeu
 
 if __name__ == "__main__":
     print("Jeu créé par Yann, François, Gauthier et Charles")
-    # nb_joueurs = 0
-    # while nb_joueurs < 2 or nb_joueurs > 6:
-    #     nb_joueurs = int(input("Choisissez le nombre de joueurs (entre 2 et 6) :"))
-
-    nb_joueurs = 2
+    nb_joueurs = 0
+    while nb_joueurs < 2 or nb_joueurs > 6:
+        nb_joueurs = int(input("Choisissez le nombre de joueurs (entre 2 et 6) :"))
 
     Jeu(nb_joueurs).lancer()
